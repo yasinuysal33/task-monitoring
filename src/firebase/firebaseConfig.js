aefb2e1a7137ffbed8ff7c6609cab52990d687a9
yasinuysal33 <uysal.e.yasin@gmail.com>
2022-09-26 23:49:17 +0300

    ignore firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+//ignore
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCbdcWaqYEnCSOAshTAofyzXp0ClB-YsAY",
